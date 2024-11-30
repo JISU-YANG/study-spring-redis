e472ac52e25e1b1122ffd1cb585becce92427cb1
JISU-YANG <jisuyang@egoist.im>
2024-11-30 15:29:37 +0900

    세션 데이터를 Redis에 저장하기

--- src/main/java/com/sparta/redis/RedisConfig.java
+++ src/main/java/com/sparta/redis/RedisConfig.java
@@ -19,4 +19,9 @@ public class RedisConfig {
         template.setValueSerializer(new GenericToStringSerializer<>(Integer.class));
         return template;
     }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return RedisSerializer.java();
+    }
 }
